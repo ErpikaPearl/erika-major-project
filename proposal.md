c87afd22ff53d31aa772ba4c537a8265dc7d3885
ErpikaPearl <112347457+ErpikaPearl@users.noreply.github.com>
2023-11-17 12:17:05 -0600

    Added in a 2d grid object. Updated proposal.

--- proposal.md
+++ proposal.md
@@ -1,15 +1,16 @@
 # Project Description
 
-A 2d platformer video game that uses semi-realistic physics. 
+A 2d platformer video game that uses semi-realistic physics. The player should be able to navigate the world.
 
 ## Needs to Have List
 - Gravity that acts on the player
 - Momentum that acts on the player
-- Realistic collisions
+- Realistic-ish collisions
 - A moving player
-- A scene that the player can interact with 
+- A scene that the player can interact with and move inside
 
 ## Nice to Have List
 - Projectile motion
 - At least one enemy
-
+- Physics that are created by me
+- good visuals (images instead of shapes)
--- sketch.js
+++ sketch.js
@@ -5,12 +5,83 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let gridOne;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  gridOne = new Grid(0);
+  gridOne.generateEmpty();
 }
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 100);
+  
+  gridOne.display();
 }
+
+class Grid {
+  constructor(xOffset){
+    this.GRID_HEIGHT = 60;
+    this.GRID_WIDTH = 120;
+    
+    this.grid = [];
+    this.cellSize = 10;
+
+    this.xOffset = xOffset;
+
+  }
+
+  generateEmpty(){
+    //  Generates an empty grid in size given
+  
+    let randomArray = [];
+    for (let y = 0; y < this.GRID_WIDTH; y++){
+      randomArray.push([]);
+      for (let x = 0; x < this.GRID_HEIGHT; x++){
+        randomArray[y].push(0);
+      }
+    }
+    this.grid = randomArray;
+  }
+  
+  display(){
+    //  fills the grid in whatever colour dependent on draw state
+    
+    for (let y = 0; y < this.GRID_HEIGHT; y++){
+      for (let x = 0; x < this.GRID_WIDTH; x++){
+        if (this.grid[y][x] === 1){
+          fill("green");
+        }
+        else if (this.grid[y][x] === 4){
+          fill("blue");
+        }
+        else if (this.grid[y][x] === 5){
+          fill("black");
+        }
+        else if (this.grid[y][x] === 6){
+          fill("aqua");
+        }
+        else if (this.grid[y][x] === 7){
+          fill("brown");
+        }
+        else{
+          fill("white");
+        }
+        rect(x*this.cellSize - this.xOffset, y*this.cellSize, this.cellSize, this.cellSize);
+      }
+    }
+  }
+}
+
+// class Veiw {
+//   constructor(){
+
+//   }
+// }
+
+// class conroller {
+//   constructor(){
+
+//   }
+// }
