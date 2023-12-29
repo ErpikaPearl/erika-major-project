ee88dc2d3ef2f738657200153b1bafdf720fcc81
ErpikaPearl <112347457+ErpikaPearl@users.noreply.github.com>
2023-12-29 16:02:41 -0600

    Finished placing all platforms and walls for the first level. Added sound effects to player movement.

--- sketch play world.js
+++ sketch play world.js
@@ -8,20 +8,37 @@
 
 //  Declaring Variables
 
-//  Declaring sprites and groups
+//  Declaring sprites, assets and groups
 let player, ground, dots, testOB, orgin, HUDFuel;
 let lvlOneBackground, levelOne;
 let cantPass;
 let solidsGroup = [];
+let footStep0, footStep1, jump, jetPack0, wind;
 //  Declaring player variables
 let playerMaxSpeed = 10;
 let doubleJump = false;
 let dash = true;
 let lastSwitchedDash = 0;
+let lastSwitchedWalk = 0;
 let isOnGround = false;
 //  Declaring world and camera variables
 let cameraMovement = 50;
 let wallWidth = 50;
+let level = "One";
+
+function preload(){
+  footStep0 = loadSound("Assets/footstep00.ogg");
+  footStep1 = loadSound("Assets/footstep01.ogg");
+  jump = loadSound("Assets/jump.wav");
+  jetPack0 = loadSound("Assets/jetpack0.wav");
+  wind = loadSound("Assets/wind.wav");
+
+  footStep0.setVolume(0.1);
+  footStep1.setVolume(0.08);
+  jump.setVolume(1);
+  jetPack0.setVolume(0.2);
+  wind.setVolume(1);
+}
 
 function setup() {
   //  Set up world
@@ -30,7 +47,7 @@ function setup() {
   rectMode(CENTER);
   world.gravity.y = 9.8;
   camera.zoom = 0.9;
-  camera.zoom = 0.1;
+  // camera.zoom = 0.1;
 
   //  Set up player
   player = new Sprite();
@@ -196,7 +213,155 @@ function setup() {
   lvlOneFloorFifthRight.height = wallWidth;
   solidsGroup.push(lvlOneFloorFifthRight);
 
-  //  Create platforms and secondary walls
+  //  Create platforms
+  let lvlOnePlaformFirst = new levelOne.Sprite();
+  lvlOnePlaformFirst.x = lvlOneBackground.x + wallWidth*66;
+  lvlOnePlaformFirst.y = lvlOneFloorBottom.y - wallWidth*3;
+  lvlOnePlaformFirst.width = wallWidth*3;
+  lvlOnePlaformFirst.height = wallWidth;
+  solidsGroup.push(lvlOnePlaformFirst);
+
+  let lvlOnePlaformThirdLeft = new levelOne.Sprite();
+  lvlOnePlaformThirdLeft.x = lvlOneBackground.x - wallWidth*25;
+  lvlOnePlaformThirdLeft.y = lvlOneFloorSecond.y - wallWidth*4;
+  lvlOnePlaformThirdLeft.width = wallWidth*35;
+  lvlOnePlaformThirdLeft.height = wallWidth;
+  solidsGroup.push(lvlOnePlaformThirdLeft);
+  let lvlOnePlaformThirdMiddle = new levelOne.Sprite();
+  lvlOnePlaformThirdMiddle.x = lvlOneBackground.x + wallWidth*54;
+  lvlOnePlaformThirdMiddle.y = lvlOneFloorSecond.y - wallWidth*4;
+  lvlOnePlaformThirdMiddle.width = wallWidth*8;
+  lvlOnePlaformThirdMiddle.height = wallWidth;
+  solidsGroup.push(lvlOnePlaformThirdMiddle);
+  let lvlOnePlaformThirdRight = new levelOne.Sprite();
+  lvlOnePlaformThirdRight.x = lvlOneBackground.x + wallWidth*56;
+  lvlOnePlaformThirdRight.y = lvlOneFloorSecond.y - wallWidth*7;
+  lvlOnePlaformThirdRight.width = wallWidth*4;
+  lvlOnePlaformThirdRight.height = wallWidth;
+  solidsGroup.push(lvlOnePlaformThirdRight);
+
+  let lvlOnePlaformFourth = new levelOne.Sprite();
+  lvlOnePlaformFourth.x = lvlOneBackground.x - wallWidth*56;
+  lvlOnePlaformFourth.y = lvlOneFloorThirdLeft.y - wallWidth*4;
+  lvlOnePlaformFourth.width = wallWidth*4;
+  lvlOnePlaformFourth.height = wallWidth;
+  solidsGroup.push(lvlOnePlaformFourth);
+
+  let lvlOnePlaformFifthMiddle = new levelOne.Sprite();
+  lvlOnePlaformFifthMiddle.x = lvlOneBackground.x;
+  lvlOnePlaformFifthMiddle.y = lvlOneFloorFourthRight.y - wallWidth*8;
+  lvlOnePlaformFifthMiddle.width = wallWidth*8;
+  lvlOnePlaformFifthMiddle.height = wallWidth;
+  solidsGroup.push(lvlOnePlaformFifthMiddle);
+  let lvlOnePlaformFifthLeft = new levelOne.Sprite();
+  lvlOnePlaformFifthLeft.x = lvlOneBackground.x - wallWidth*8;
+  lvlOnePlaformFifthLeft.y = lvlOneFloorFourthRight.y - wallWidth*5;
+  lvlOnePlaformFifthLeft.width = wallWidth*5;
+  lvlOnePlaformFifthLeft.height = wallWidth;
+  solidsGroup.push(lvlOnePlaformFifthLeft);
+  let lvlOnePlaformFifthRight = new levelOne.Sprite();
+  lvlOnePlaformFifthRight.x = -(lvlOnePlaformFifthLeft.x);
+  lvlOnePlaformFifthRight.y = lvlOneFloorFourthRight.y - wallWidth*5;
+  lvlOnePlaformFifthRight.width = wallWidth*5;
+  lvlOnePlaformFifthRight.height = wallWidth;
+  solidsGroup.push(lvlOnePlaformFifthRight);
+  let lvlOnePlaformFifthRightmost0 = new levelOne.Sprite();
+  lvlOnePlaformFifthRightmost0.x = lvlOneBackground.x + wallWidth*42;
+  lvlOnePlaformFifthRightmost0.y = lvlOneFloorFourthRight.y - wallWidth*6;
+  lvlOnePlaformFifthRightmost0.width = wallWidth*41;
+  lvlOnePlaformFifthRightmost0.height = wallWidth;
+  solidsGroup.push(lvlOnePlaformFifthRightmost0);
+  let lvlOnePlaformFifthRightmost1 = new levelOne.Sprite();
+  lvlOnePlaformFifthRightmost1.x = lvlOneBackground.x + wallWidth*68;
+  lvlOnePlaformFifthRightmost1.y = lvlOneFloorFourthRight.y - wallWidth*3;
+  lvlOnePlaformFifthRightmost1.width = wallWidth*5;
+  lvlOnePlaformFifthRightmost1.height = wallWidth;
+  solidsGroup.push(lvlOnePlaformFifthRightmost1);
+  let lvlOnePlaformFifthLeftmost = new levelOne.Sprite();
+  lvlOnePlaformFifthLeftmost.x = lvlOneBackground.x - wallWidth*65;
+  lvlOnePlaformFifthLeftmost.y = lvlOneFloorFourthRight.y - wallWidth*4;
+  lvlOnePlaformFifthLeftmost.width = wallWidth*4;
+  lvlOnePlaformFifthLeftmost.height = wallWidth;
+  solidsGroup.push(lvlOnePlaformFifthLeftmost);
+
+  //   Create secondary walls
+  let lvlOneWallFirst0 = new levelOne.Sprite();
+  lvlOneWallFirst0.x = lvlOneBackground.x - wallWidth*60;
+  lvlOneWallFirst0.y = (lvlOneFloorBottom.y + lvlOneFloorFirstLeft.y)/2 - wallWidth*1;
+  lvlOneWallFirst0.width = wallWidth;
+  lvlOneWallFirst0.height = wallWidth*3;
+  let lvlOneWallFirst1 = new levelOne.Sprite();
+  lvlOneWallFirst1.x = lvlOneWallFirst0.x + wallWidth*15;
+  lvlOneWallFirst1.y = lvlOneWallFirst0.y;
+  lvlOneWallFirst1.width = wallWidth;
+  lvlOneWallFirst1.height = lvlOneWallFirst0.height;
+  let lvlOneWallFirst2 = new levelOne.Sprite();
+  lvlOneWallFirst2.x = lvlOneWallFirst0.x + wallWidth*55;
+  lvlOneWallFirst2.y = lvlOneWallFirst0.y;
+  lvlOneWallFirst2.width = wallWidth;
+  lvlOneWallFirst2.height = lvlOneWallFirst0.height;
+  let lvlOneWallFirst3 = new levelOne.Sprite();
+  lvlOneWallFirst3.x = lvlOneBackground.x + wallWidth*45;
+  lvlOneWallFirst3.y = lvlOneWallFirst0.y;
+  lvlOneWallFirst3.width = wallWidth;
+  lvlOneWallFirst3.height = lvlOneWallFirst0.height;
+
+  let lvlOneWallSecond0 = new levelOne.Sprite();
+  lvlOneWallSecond0.x = lvlOneBackground.x - wallWidth*40;
+  lvlOneWallSecond0.y = (lvlOneFloorFirstLeft.y + lvlOneFloorSecond.y)/2 - wallWidth*1;
+  lvlOneWallSecond0.width = wallWidth;
+  lvlOneWallSecond0.height = wallWidth*2;
+  let lvlOneWallSecond1 = new levelOne.Sprite();
+  lvlOneWallSecond1.x =  lvlOneWallSecond0.x + wallWidth*30;
+  lvlOneWallSecond1.y = lvlOneWallSecond0.y;
+  lvlOneWallSecond1.width = wallWidth;
+  lvlOneWallSecond1.height = wallWidth*2;
+  let lvlOneWallSecond2 = new levelOne.Sprite();
+  lvlOneWallSecond2.x =  lvlOneWallSecond0.x + wallWidth*70;
+  lvlOneWallSecond2.y = lvlOneWallSecond0.y;
+  lvlOneWallSecond2.width = wallWidth;
+  lvlOneWallSecond2.height = wallWidth*2;
+
+  let lvlOneWallThird0 = new levelOne.Sprite();
+  lvlOneWallThird0.x = lvlOneBackground.x - wallWidth*25;
+  lvlOneWallThird0.y = (lvlOneFloorSecond.y + lvlOneFloorThirdLeft.y)/2 - wallWidth*2;
+  lvlOneWallThird0.width = wallWidth;
+  lvlOneWallThird0.height = wallWidth*5;
+  let lvlOneWallThird1 = new levelOne.Sprite();
+  lvlOneWallThird1.x = lvlOneBackground.x + wallWidth*58;
+  lvlOneWallThird1.y = lvlOneWallThird0.y;
+  lvlOneWallThird1.width = wallWidth;
+  lvlOneWallThird1.height = wallWidth*7;
+
+  let lvlOneWallFourth0 = new levelOne.Sprite();
+  lvlOneWallFourth0.x = lvlOneBackground.x - wallWidth*58;
+  lvlOneWallFourth0.y = (lvlOneFloorThirdLeft.y + lvlOneFloorFourthLeft.y)/2 - wallWidth*2;
+  lvlOneWallFourth0.width = wallWidth;
+  lvlOneWallFourth0.height = wallWidth*3;
+  let lvlOneWallFourth1 = new levelOne.Sprite();
+  lvlOneWallFourth1.x = lvlOneBackground.x - wallWidth*20;
+  lvlOneWallFourth1.y = (lvlOneFloorThirdLeft.y + lvlOneFloorFourthLeft.y)/2 - wallWidth*1;
+  lvlOneWallFourth1.width = wallWidth;
+  lvlOneWallFourth1.height =lvlOneWallFourth0.height;
+  let lvlOneWallFourth2 = new levelOne.Sprite();
+  lvlOneWallFourth2.x = lvlOneBackground.x + wallWidth*20;
+  lvlOneWallFourth2.y = (lvlOneFloorThirdLeft.y + lvlOneFloorFourthLeft.y)/2 - wallWidth*1;
+  lvlOneWallFourth2.width = wallWidth;
+  lvlOneWallFourth2.height = lvlOneWallFourth0.height;
+
+  let lvlOneWallFifth0 = new levelOne.Sprite();
+  lvlOneWallFifth0.x = lvlOneBackground.x - wallWidth*63;
+  lvlOneWallFifth0.y = (lvlOneFloorFourthLeft.y + lvlOneFloorFifthLeft.y)/2 - wallWidth*1;
+  lvlOneWallFifth0.width = wallWidth;
+  lvlOneWallFifth0.height = wallWidth*10;
+  let lvlOneWallFifth1 = new levelOne.Sprite();
+  lvlOneWallFifth1.x = lvlOneBackground.x + wallWidth*22;
+  lvlOneWallFifth1.y = lvlOneWallFifth0.y; 
+  lvlOneWallFifth1.width = wallWidth;
+  lvlOneWallFifth1.height = wallWidth*10;
+
+  player.y = -lvlOneBackground.height/2 - wallWidth*1.5;
+  player.x = lvlOneBackground.width/2 - wallWidth*5;
 }
 
 function draw() {
@@ -217,14 +382,21 @@ function draw() {
   HUDFuel.draw();
 }
 
+function mousePressed(){
+  if(!wind.isPlaying() && level === "One"){
+    wind.loop();
+  }
+}
+
 function keyPressed(){
   //  Player Movements
   if (keyCode === 32){  //  (SPACE) Jump
     if (isOnGround){
-      player.applyForceScaled(0, -400);
-      
+      jump.play();
+      player.applyForceScaled(0, -400);      
     }
     else if (doubleJump && !isOnGround){
+      jetPack0.play();
       player.applyForceScaled(0, -300);
       doubleJump = false;
     }
@@ -234,6 +406,7 @@ function keyPressed(){
     let waitTime = 1000;
     
     if (millis() > waitTime + lastSwitchedDash){
+      jetPack0.play();
       if (player.bearing === 360){
         player.applyForceScaled(800, 0);
       }
@@ -266,6 +439,7 @@ function detectPlayerImput(){
     player.bearing = 180;
     if (isOnGround){
       player.applyForceScaled(-50, 0);
+      walkSound();
     }
     else{
       player.applyForceScaled(-5, 0);
@@ -275,6 +449,7 @@ function detectPlayerImput(){
     player.bearing = 360;
     if (isOnGround){
       player.applyForceScaled(50, 0);
+      walkSound();
     }
     else{
       player.applyForceScaled(5, 0);
@@ -297,11 +472,29 @@ function managePlayerStates(){
       isOnGround = true;
     }
   }
+  // if (player.y >= 0){
+  //   wind.setVolume(1);
+  //   console.log("wind");
+  // }
 }
 
-function coolDown(waitTime, lastSwitched, ability){
-  if (millis() > waitTime + lastSwitched){
-    ability = !ability;
-    lastSwitched = millis();
+function walkSound(){
+  let played = 0;
+  if (played === 0 && !footStep0.isPlaying()){
+    footStep0.play();
+    played ++;
+    if (played === 1 && !footStep1.isPlaying()){
+      footStep1.play();
+      played ++;
+    }
   }
 }
+
+
+
+// function coolDown(waitTime, lastSwitched, ability){
+//   if (millis() > waitTime + lastSwitched){
+//     ability = !ability;
+//     lastSwitched = millis();
+//   }
+// }
